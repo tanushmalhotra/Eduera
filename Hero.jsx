8c9cca4cb36104cf36dafa2d2e40de85b2d72919
tanushmalhotra <146076078+tanushmalhotra@users.noreply.github.com>
2024-08-07 17:16:17 +0530

    Create Hero.jsx

---
+++ Hero.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Hero.css'
+import dark_arrow from '../../assets/dark-arrow.png'
+
+const Hero = () => {
+  return (
+    <div className='hero conatiner'>
+      <div className='hero-text'>
+      <h1>We Ensure better education for a better tomorrow.</h1>
+      <p> Our cutting-edge curriculum is designed to empower 
+        students with the knowledge, skills, and experience needed 
+        to excel in the dynamic field of education.</p>
+        <button className='btn'>Explore more <img src={dark_arrow} alt="" /></button>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
